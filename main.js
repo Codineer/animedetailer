5979d60b12c087f1383bb810b26c73daa8b2a7cc
codineer <utkarshgamer294@outlook.com>
2024-10-04 16:54:46 +0530

    commit

--- main.js
+++ main.js
@@ -1,23 +1,29 @@
 import express from 'express'
 import path from 'path';
 import { fileURLToPath } from 'url';
+import router from './routes/anime/anime.js';
 
 const app = express()
+app.set('view engine', 'ejs');
 const port = 3000
 const __filename = fileURLToPath(import.meta.url);
 const __dirname = path.dirname(__filename);
 
 app.use(express.static('public'))
-app.get('/', (req, res) => {
+app.use('/anime', router)
 
-    res.send('Hello World!')
-})
+app.get('/', async (req, res) => {
+    const data1 = await fetch('https://kitsu.io/api/edge/trending/anime')
+    const data2 = await fetch('https://kitsu.io/api/edge/anime?sort=-userCount')
+    const data3 = await fetch('https://kitsu.io/api/edge/anime?sort=-favoritesCount')
 
-app.post('/', (req, res) => {
-    res.send('Hello World')
+    const TrendingAnimes = await data1.json()
+    const PopularAnimes = await data2.json()
+    const favoritesAnimes = await data3.json()
+    res.render('index', { TrendingAnimes, PopularAnimes, favoritesAnimes })
 })
 app.get('/index', (req, res) => {
-    res.sendFile('templates/index.html', { root: __dirname })
+    res.send('ss')
 })
 
 app.get('/blog/:slug', (req, res) => {
---
+++ public/javascript/main.js
@@ -0,0 +1,54 @@
+var swiper = new Swiper(".home", {
+    spaceBetween: 30,
+    centeredSlides: true,
+    autoplay: {
+        delay: 4000,
+        disableOnInteraction: false,
+    },
+    pagination: {
+        el: ".swiper-pagination",
+        clickable: true,
+    },
+
+});
+
+let header = document.querySelector('nav');
+let menu = document.querySelector('#menu-icon');
+let search = document.querySelector('#search-icon');
+let navbar = document.querySelector('.navbar');
+let searchform = document.querySelector('.search-form');
+let navbarItems = document.querySelectorAll('.navitem');
+
+
+menu.addEventListener("click", () => {
+    console.log('Menu icon clicked');
+
+
+    if (navbar.style.display === 'none' || navbar.style.display === '') {
+        navbar.style.display = 'block';
+        navbar.classList.add("mystylenav");// Show the navbar
+        for (const navbarItem of navbarItems) {
+            console.log(navbarItem)
+            navbarItem.classList.add("media-ul");
+
+        }
+        console.log('Navbar shown');
+    } else {
+        navbar.style.display = 'none';  // Hide the navbar
+        console.log('Navbar hidden');
+    }
+});
+search.addEventListener("click", () => {
+    console.log('Menu icon clicked');
+
+
+    if (searchform.style.display === 'none' || searchform.style.display === '') {
+        searchform.style.display = 'block';
+        searchform.classList.add("mystylesearch");// Show the navbar
+
+        // console.log('Navbar shown');
+    } else {
+        searchform.style.display = 'none';  // Hide the navbar
+
+    }
+});
