9bd23586890137afa7a0392ccf9736e8f8b55848
codineer <utkarshgamer294@outlook.com>
2024-10-07 09:41:27 +0530

    commit

--- routes/anime/anime.js
+++ routes/anime/anime.js
@@ -5,31 +5,32 @@ const router = Router()
 
 router.get('/:slug', async (req, res) => {
 
-    // res.send(req.params.slug)
-    let resp = await fetch(`https://kitsu.io/api/edge/anime/${req.params.slug}`)
-    resp = await resp.json()
-    let episodeslink = resp.data.relationships.episodes.links.self
-    let animecharacterslink = resp.data.relationships.animeCharacters.links.self
 
-    let animech = await fetch(animecharacterslink)
-    animech = await animech.json()
+    // let resp = await fetch(`https://kitsu.io/api/edge/anime/${req.params.slug}`)
+    // resp = await resp.json()
+    // let episodeslink = resp.data.relationships.episodes.links.self
+    // let animecharacterslink = resp.data.relationships.animeCharacters.links.self
 
-    let animeCharacters = []
-    for (const ch of animech.data) {
-        try {
-            let character = await fetch(`https://kitsu.io/api/edge/characters/${ch.id}`)
+    // let animech = await fetch(animecharacterslink)
+    // animech = await animech.json()
 
-            character = await character.json()
-            console.log(character)
-            animeCharacters.push(character)
-        }
-        catch (e) {
-            console.log(e)
-            continue
-        }
-    }
-    console.log(animeCharacters)
-    res.send("hello world")
+    // let animeCharacters = []
+    // for (const ch of animech.data) {
+    //     try {
+    //         let character = await fetch(`https://kitsu.io/api/edge/characters/${ch.id}`)
+
+    //         character = await character.json()
+    //         console.log(character)
+    //         animeCharacters.push(character)
+    //     }
+    //     catch (e) {
+    //         console.log(e)
+    //         continue
+    //     }
+    // }
+    // console.log(animeCharacters)
+
+    res.render('anime/animepage')
 })
 
 export default router
