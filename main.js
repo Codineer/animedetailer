818cf37640bb68e13dc4bdbeb86238cd24f60576
codineer <utkarshgamer294@outlook.com>
2024-10-11 22:20:34 +0530

    commit

--- main.js
+++ main.js
@@ -9,14 +9,13 @@ let kitsuApi = new KitsuApi();
 const app = express()
 app.set('view engine', 'ejs');
 const port = 3000
-const __filename = fileURLToPath(import.meta.url);
-const __dirname = path.dirname(__filename);
+
 
 app.use(express.static('public'))
 app.use('/anime', router)
 
 app.get('/', async (req, res) => {
-    console.log('hii anshu')
+
     const data1 = await fetch('https://kitsu.io/api/edge/trending/anime')
     const data2 = await fetch('https://kitsu.io/api/edge/anime?sort=-userCount')
     const data3 = await fetch('https://kitsu.io/api/edge/anime?sort=-favoritesCount')
--- routes/anime/anime.js
+++ routes/anime/anime.js
@@ -8,10 +8,32 @@ router.get('/categories', async (req, res) => {
     res.render('anime/categories', { categories })
 
 })
+router.get('/categories/:slug', async (req, res) => {
+
+    let resp = await fetch(`https://kitsu.io/api/edge/anime?filter[categories]=${req.params.slug}`)
+    let animes = await resp.json()
+    res.render('anime/category', { animes, categoryName: req.query.name })
+
+})
 router.get('/:slug', async (req, res) => {
 
     let resp = await fetch(`https://kitsu.io/api/edge/anime/${req.params.slug}`)
     let animedata = await resp.json()
+    let streamingServicesImages = ['amazon', 'animnelab', 'contv', 'crunchyroll', 'funimation', 'hidive', 'hulu', 'netflix', 'tubi', 'youtube']
+    let streamingData = await fetch(`https://kitsu.io/api/edge/anime/${req.params.slug}/streaming-links`)
+    streamingData = await streamingData.json()
+    let streamingServices = []
+    for (const streamer of streamingData.data) {
+        const url = streamer.attributes.url.toLowerCase();
+        for (const imageName of streamingServicesImages) {
+            if (url.includes(imageName)) {
+                streamingServices.push({
+                    image: `${imageName}.png`,
+                    url: streamer.attributes.url
+                })
+            }
+        }
+    }
 
     let animecharacterslink = animedata.data.relationships.animeCharacters.links.related
 
@@ -34,7 +56,7 @@ router.get('/:slug', async (req, res) => {
     }
 
 
-    res.render('anime/animepage', { animedata, animeCharacters })
+    res.render('anime/animepage', { animedata, animeCharacters, streamingServices })
 })
 router.get('/episodes/:slug', async (req, res) => {
     let resp = await fetch(`https://kitsu.io/api/edge/anime/${req.params.slug}/relationships/episodes`)
