9162ed0f99cca6ba9e3b098616831bd414e5829b
codineer <utkarshgamer294@outlook.com>
2024-10-11 13:57:52 +0530

    commit

--- routes/anime/anime.js
+++ routes/anime/anime.js
@@ -1,6 +1,13 @@
 import { Router } from "express";
 const router = Router()
 
+router.get('/categories', async (req, res) => {
+
+    let resp = await fetch(`https://kitsu.io/api/edge/categories`)
+    let categories = await resp.json()
+    res.render('anime/categories', { categories })
+
+})
 router.get('/:slug', async (req, res) => {
 
     let resp = await fetch(`https://kitsu.io/api/edge/anime/${req.params.slug}`)
@@ -47,6 +54,13 @@ router.get('/character/:slug', async (req, res) => {
     let character = await resp.json()
 
     res.render('anime/character', { character })
+})
+router.get('/genre/:slug', async (req, res) => {
+
+    let resp = await fetch(`https://kitsu.io/api/edge/anime?filter[genres]=${req.params.slug}`)
+    let animes = await resp.json()
+
+    res.render('anime/genrepage', { animes, genreName: req.params.slug })
 })
 
 export default router
