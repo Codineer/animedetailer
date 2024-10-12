f89fe5fbd96ec6edb872099369c04067f23123dd
codineer <utkarshgamer294@outlook.com>
2024-10-13 01:53:49 +0530

    commit

--- index.js
+++ index.js
@@ -7,6 +7,7 @@ import KitsuApi from 'kitsu-json-api';
 
 let kitsuApi = new KitsuApi();
 const app = express()
+app.set('views', path.join('./views', 'views'));
 app.set('view engine', 'ejs');
 const port = 3000
 
